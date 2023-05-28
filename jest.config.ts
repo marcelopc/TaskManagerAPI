da8ea085dddbb3f2f32367e706d53659405a594d
marcelo <marcelopc04@gmail.com>
2023-05-28 13:17:15 -0300

    chore:jest

--- jest.config.ts
+++ jest.config.ts
@@ -54,6 +54,7 @@ export default {
   // fakeTimers: {
   //   "enableGlobally": false
   // },
+ 
 
   // Force coverage collection from ignored files using an array of glob patterns
   // forceCoverageMatch: [],
@@ -129,9 +130,9 @@ export default {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    "./src"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -162,6 +163,7 @@ export default {
   //   "**/__tests__/**/*.[jt]s?(x)",
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
+  testMatch: ['**/*.test.ts'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
