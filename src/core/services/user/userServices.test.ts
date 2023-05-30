eff219236651ab29b07542434779ff72eb637504
marcelo <marcelopc04@gmail.com>
2023-05-30 09:41:28 -0300

    test: testes para login

--- src/core/services/user/userServices.test.ts
+++ src/core/services/user/userServices.test.ts
@@ -2,7 +2,7 @@ import userServices from './userServices'
 import { type PayloadCreateUserType, type CreateUserType } from '@core/types/user/userTypes'
 import { type UserModel } from '@core/types/user/userModel'
 import crypto from '@infrastructure/utils/crypt'
-
+import { newError } from '@core/util/error'
 import dotenv from 'dotenv'
 import dotenvExpand from 'dotenv-expand'
 dotenvExpand.expand(dotenv.config())
@@ -32,94 +32,157 @@ const userModel = {
   },
   findOne: async (field: keyof UserModel, value: string | Date): Promise<UserModel | null> => {
     const user: UserModel = {
-      id: 'anyemail',
-      nome: 'anyemail',
+      id: 'anyid',
+      nome: 'anynome',
       email: 'anyemail',
-      password: 'anyemail',
+      password: 'anypassword',
       createdAt: date,
       updatedAt: date
     }
+
     return user
   }
 }
 
-it('Retornando erro se não informar nome', async () => {
-  const payload = {
-    nome: '',
-    email: 'anyemail',
-    password: 'anypassword'
-  }
+const fakeUserModel = {
+  register: async (payload: PayloadCreateUserType): Promise<UserModel> => {
+    const user: UserModel = {
+      id: 'anyid',
+      nome: payload.nome,
+      email: payload.email,
+      password: payload.password,
+      createdAt: date,
+      updatedAt: date
+    }
 
-  try {
-    await userServices.createUser(payload, userModel, crypto)
-  } catch (error) {
-    expect(error).toEqual(new Error('nome, email e senha são obrigatários'))
-  }
+    return user
+  },
+  findOne: async (field: keyof UserModel, value: string | Date): Promise<UserModel | null> => null
+}
+
+describe('createUser', () => {
+  it('Retornando erro se não informar nome', async () => {
+    const payload = {
+      nome: '',
+      email: 'anyemail',
+      password: 'anypassword'
+    }
+
+    try {
+      await userServices.createUser(payload, userModel, crypto)
+    } catch (error) {
+      expect(error).toEqual(newError(400, 'nome, email e senha são obrigatários'))
+    }
+  })
+
+  it('Retornando erro se não informar email', async () => {
+    const payload = {
+      nome: 'anyname',
+      email: '',
+      password: 'anypassword'
+    }
+
+    try {
+      await userServices.createUser(payload, userModel, crypto)
+    } catch (error) {
+      expect(error).toEqual(newError(400, 'nome, email e senha são obrigatários'))
+    }
+  })
+
+  it('Retornando erro se não informar password', async () => {
+    const payload = {
+      nome: 'anyname',
+      email: 'anyemail',
+      password: ''
+    }
+
+    try {
+      await userServices.createUser(payload, userModel, crypto)
+    } catch (error) {
+      expect(error).toEqual(newError(400, 'nome, email e senha são obrigatários'))
+    }
+  })
+
+  it('Retornando erro se informar password menor que 8 caracteres', async () => {
+    const payload = {
+      nome: 'anyname',
+      email: 'anyemail',
+      password: 'any'
+    }
+
+    try {
+      await userServices.createUser(payload, userModel, crypto)
+    } catch (error) {
+      expect(error).toEqual(newError(400, 'Senha deve ter pelo menos 8 caracteres'))
+    }
+  })
+
+  it('Retornando erro se informar password maior que 20 caracteres', async () => {
+    const payload = {
+      nome: 'anyname',
+      email: 'anyemail',
+      password: 'anypasswordanypasswordanypasswordanypassword'
+    }
+
+    try {
+      await userServices.createUser(payload, userModel, crypto)
+    } catch (error) {
+      expect(error).toEqual(newError(400, 'Senha deve ter no máximo 20 caracteres'))
+    }
+  })
+
+  it('Criando usuario com sucesso', async () => {
+    const payload: PayloadCreateUserType = {
+      nome: 'anyname',
+      email: 'anyemail',
+      password: 'anypassword'
+    }
+    const user = await userServices.createUser(payload, fakeUserModel, crypto)
+    expect(user).toEqual(userDatabase)
+  })
 })
 
-it('Retornando erro se não informar email', async () => {
-  const payload = {
-    nome: 'anyname',
-    email: '',
-    password: 'anypassword'
-  }
+describe('loginUser', () => {
+  it('Retornando token com sucesso', async () => {
+    const payload = {
+      email: 'anyemail',
+      password: 'anypassword'
+    }
+    const token = await userServices.login(payload, userModel)
+    expect(token).toEqual('anytoken')
+  })
+  it('Retornando erro se não informar email', async () => {
+    const payload = {
+      email: '',
+      password: 'anypassword'
+    }
+    try {
+      await userServices.login(payload, userModel)
+    } catch (error) {
+      expect(error).toEqual(newError(400, 'email e senha são obrigatários'))
+    }
+  })
+  it('Retornando erro se não informar senha', async () => {
+    const payload = {
+      email: 'anyemail',
+      password: ''
+    }
+    try {
+      await userServices.login(payload, userModel)
+    } catch (error) {
+      expect(error).toEqual(newError(400, 'email e senha são obrigatários'))
+    }
+  })
 
-  try {
-    await userServices.createUser(payload, userModel, crypto)
-  } catch (error) {
-    expect(error).toEqual(new Error('nome, email e senha são obrigatários'))
-  }
-})
-
-it('Retornando erro se não informar password', async () => {
-  const payload = {
-    nome: 'anyname',
-    email: 'anyemail',
-    password: ''
-  }
-
-  try {
-    await userServices.createUser(payload, userModel, crypto)
-  } catch (error) {
-    expect(error).toEqual(new Error('nome, email e senha são obrigatários'))
-  }
-})
-
-it('Retornando erro se informar password menor que 8 caracteres', async () => {
-  const payload = {
-    nome: 'anyname',
-    email: 'anyemail',
-    password: 'any'
-  }
-
-  try {
-    await userServices.createUser(payload, userModel, crypto)
-  } catch (error) {
-    expect(error).toEqual(new Error('Senha deve ter pelo menos 8 caracteres'))
-  }
-})
-
-it('Retornando erro se informar password maior que 20 caracteres', async () => {
-  const payload = {
-    nome: 'anyname',
-    email: 'anyemail',
-    password: 'anypasswordanypasswordanypasswordanypassword'
-  }
-
-  try {
-    await userServices.createUser(payload, userModel, crypto)
-  } catch (error) {
-    expect(error).toEqual(new Error('Senha deve ter no máximo 20 caracteres'))
-  }
-})
-
-it('Criando usuario com sucesso', async () => {
-  const payload: PayloadCreateUserType = {
-    nome: 'anyname',
-    email: 'anyemail',
-    password: 'anypassword'
-  }
-  userModel.findOne = async (field: keyof UserModel, value: string | Date): Promise<UserModel | null> => null
-  const user = await userServices.createUser(payload, userModel, crypto)
-  expect(user).toEqual(userDatabase)
+  it('Retornando erro se não encontrar usuario', async () => {
+    const payload = {
+      email: 'anyemail',
+      password: 'anypassword'
+    }
+    try {
+      await userServices.login(payload, userModel)
+    } catch (error) {
+      expect(error).toEqual(newError(400, 'email e senha são obrigatários'))
+    }
+  })
 })
