764cc3b84e6c413651a105db7f5f9be539c17f7d
marcelo <marcelopc04@gmail.com>
2023-05-31 17:07:28 -0300

    feat: type decod

--- src/core/types/jwtGenerator/index.ts
+++ src/core/types/jwtGenerator/index.ts
@@ -1,7 +1,18 @@
 export interface PayloadJwt {
   id: string
 }
+export interface JwtPayload {
+  id: string
+  iss?: string | undefined
+  sub?: string | undefined
+  aud?: string | string[] | undefined
+  exp?: number | undefined
+  nbf?: number | undefined
+  iat?: number | undefined
+  jti?: string | undefined
+}
 
 export interface JwtGenerator {
   create: (payload: PayloadJwt) => string
+  decode: (token: string) => JwtPayload | null
 }
