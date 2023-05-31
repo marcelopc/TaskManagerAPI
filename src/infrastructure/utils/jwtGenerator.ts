195e36542947887f7d9a3801edbe5b30e58add00
marcelo <marcelopc04@gmail.com>
2023-05-31 17:06:31 -0300

    feat: decod token

--- src/infrastructure/utils/jwtGenerator.ts
+++ src/infrastructure/utils/jwtGenerator.ts
@@ -1,9 +1,13 @@
-import { type JwtGenerator, type PayloadJwt } from '@core/types/jwtGenerator'
+import { type JwtGenerator, type PayloadJwt, type JwtPayload } from '@core/types/jwtGenerator'
 import jwt from 'jsonwebtoken'
 
 export const jwtGenerator: JwtGenerator = {
   create: (payload: PayloadJwt): string => {
     const token = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: '1d' })
     return token
+  },
+  decode: (token: string): JwtPayload | null => {
+    const decoded = jwt.decode(token)
+    return decoded as JwtPayload
   }
 }
